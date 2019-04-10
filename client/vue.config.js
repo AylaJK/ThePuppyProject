191655c706dcf400ff2c0c5ea0bd2600c9301b69
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2019-04-10 17:20:50 -0600

    Add Pug Pre-Processor

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,5 +3,25 @@ const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 module.exports = {
   configureWebpack: {
     plugins: [new MomentLocalesPlugin()]
+  },
+  chainWebpack: config => {
+    // Pug Pre-Processor
+    config.module
+      .rule("pug")
+      .test(/\.pug$/)
+      .oneOf("vuetemplate")
+        .resourceQuery(/^\?vue/)
+        .use("pug-plain-loader")
+          .loader("pug-plain-loader")
+          .end()
+        .end()
+      .oneOf("javascript")
+        .use("raw-loader")
+          .loader("raw-loader")
+          .end()
+        .use("pug-plain-loader")
+          .loader("pug-plain-loader")
+          .end()
+        .end()
   }
 };
